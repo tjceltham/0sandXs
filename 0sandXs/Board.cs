0a6841333798f09b662598abe14d7ec47c729fc0
Tony Collins <tjc@eltham-college.org.uk>
2021-03-04 12:23:01 +0000

    Add project files.

---
+++ 0sandXs/Board.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0sandXs
+{
+
+    
+    class Board
+    {
+        // private string[,] or string[]
+
+        public Board()
+        {
+            // Add " " to each position board
+        }
+
+
+        public void displayBoard() 
+        {
+            Console.WriteLine("|" + "|");
+            Console.WriteLine("|" + "|");
+            Console.WriteLine("|" + "|");
+        }
+
+        public bool addCounter(int player, int position)
+        {
+
+        }
+
+        public int checkwin()
+        {
+            // 0 - no Win
+            // 1- Player 1 wins
+            // 2 - Player 2 wins
+            // 3 - Draw
+
+        }
+
+    }
+}
---
+++ 0sandXs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _0sandXs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Board b = new Board();
+
+        }
+    }
+}
